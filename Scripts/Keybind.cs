fc3262cb7206ac6c548648909568f15493d949cc
It's Networking <87145183+Its-Networking@users.noreply.github.com>
2022-08-05 14:02:35 -0400

    Update Keybind.cs

--- Scripts/Keybind.cs
+++ Scripts/Keybind.cs
@@ -8,23 +8,27 @@ public class Keybind : MonoBehaviour
     public Camera
         loginCam,
         registerCam,
-        userInfoCam;
+        userInfoCam,
+        registerNoKeyCam;
 
     [Header("Panels")]
     public GameObject
         loginPanel,
         registerPanel,
-        userInfoPanel;
+        userInfoPanel,
+        registerNoKeyPanel;
 
     private void Start()
     {
         loginCam.enabled = true;
         registerCam.enabled = false;
         userInfoCam.enabled = false;
+        registerNoKeyCam.enabled = false;
 
         loginPanel.SetActive(true);
         registerPanel.SetActive(false);
         userInfoPanel.SetActive(false);
+        registerNoKeyPanel.SetActive(false);
     }
 
     public void Update()
@@ -34,10 +38,12 @@ public class Keybind : MonoBehaviour
             loginCam.enabled = true;
             registerCam.enabled = false;
             userInfoCam.enabled = false;
+            registerNoKeyCam.enabled = false;
 
             loginPanel.SetActive(true);
             registerPanel.SetActive(false);
             userInfoPanel.SetActive(false);
+            registerNoKeyPanel.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -45,10 +51,12 @@ public class Keybind : MonoBehaviour
             loginCam.enabled = false;
             registerCam.enabled = true;
             userInfoCam.enabled = false;
+            registerNoKeyCam.enabled = false;
 
             loginPanel.SetActive(false);
             registerPanel.SetActive(true);
             userInfoPanel.SetActive(false);
+            registerNoKeyPanel.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
@@ -56,10 +64,25 @@ public class Keybind : MonoBehaviour
             loginCam.enabled = false;
             registerCam.enabled = false;
             userInfoCam.enabled = true;
+            registerNoKeyCam.enabled = false;
 
             loginPanel.SetActive(false);
             registerPanel.SetActive(false);
+            registerNoKeyPanel.SetActive(false);
             userInfoPanel.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            loginCam.enabled = false;
+            registerCam.enabled = false;
+            userInfoCam.enabled = false;
+            registerNoKeyCam.enabled = true;
+
+            loginPanel.SetActive(false);
+            registerPanel.SetActive(false);
+            registerNoKeyPanel.SetActive(true);
+            userInfoPanel.SetActive(false);
         }
     }
 }
